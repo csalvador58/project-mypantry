fa06c56a89059f22ab1900dd34381e7f7abeb045
Chris Salvador <csalvador58@gmail.com>
2023-07-15 14:46:00 -0700

    update config

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -2,16 +2,17 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
 const mongoose_1 = __importDefault(require("mongoose"));
 require("dotenv/config");
-const port = process.env['PORT'] || 3000;
+const port = parseInt((_a = process.env['PORT']) !== null && _a !== void 0 ? _a : '3000');
 const mongoURL = process.env['MONGODB_URL'] || 'mongodb://127.0.0.1/mypantry';
 mongoose_1.default
     .connect(mongoURL, {})
     .then(() => {
-    app_1.default.listen(port, () => {
+    app_1.default.listen(port, process.env['MONGODB_IP'] || '0.0.0.0', () => {
         console.log(`App server is listening on http://localhost:${port}`);
     });
 })
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,13 +2,13 @@ import app from './app';
 import mongoose from 'mongoose';
 import 'dotenv/config';
 
-const port = process.env['PORT'] || 3000;
+const port = parseInt(process.env['PORT'] ?? '3000');
 const mongoURL = process.env['MONGODB_URL'] || 'mongodb://127.0.0.1/mypantry'; 
 
 mongoose
   .connect(mongoURL, {})
   .then(() => {
-    app.listen(port, () => {
+    app.listen(port, process.env['MONGODB_IP'] || '0.0.0.0', () => {
       console.log(`App server is listening on http://localhost:${port}`);
     });
   })
